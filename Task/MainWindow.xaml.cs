c243dd7ee2fcc73786ea663c75cd3500f9996dd0
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-03-19 14:15:37 +0500

    Task1 Complite

--- Task/MainWindow.xaml.cs
+++ Task/MainWindow.xaml.cs
@@ -12,17 +12,127 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfTaskApp.Services;
 
-namespace Task
+namespace WpfTaskApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public FileSystemVisitor _fileSystemVisitor;
+        public List<string> directories = new List<string>();
+        public List<string> files = new List<string>();
+        private string path;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Find_Click(object sender, RoutedEventArgs e)
+        {
+            path = Path.Text;
+            _fileSystemVisitor = new FileSystemVisitor();
+
+            fillLists(path);
+
+            displayDirectoryAndFileList();
+
+
+
+        }
+
+        private void fillLists(string path)
+        {
+            directories.Clear();
+            files.Clear();
+            foreach (string directory in _fileSystemVisitor.FindFolders(path))
+            {
+                directories.Add(directory);
+            }
+
+            foreach (string file in _fileSystemVisitor.FindFiles(path))
+            {
+                files.Add(file);
+            }
+
+        }
+
+        private void displayDirectoryAndFileList()
+        {
+            listofDirectories.Items.Clear();
+            listofFiles.Items.Clear();
+            foreach (string directory in directories)
+            {
+                listofDirectories.Items.Add(directory);
+            }
+
+            foreach (string file in files)
+            {
+                listofFiles.Items.Add(file);
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            _fileSystemVisitor = new FileSystemVisitor(search_directory.Text, search_file.Text);
+            path = Path.Text;
+            fillLists(path);
+
+            if (search_directory.Text != "Directory Name" && search_directory.Text != "")
+            {          
+
+                directories = _fileSystemVisitor.SearchDirectory(directories);
+                
+            }           
+
+            if (search_file.Text != "File Name" && search_file.Text != "")
+            {
+
+                files = _fileSystemVisitor.SearchFiles(files);
+                
+            }
+           
+
+
+            displayDirectoryAndFileList();
+
+
+        }
+
+        private void search_directory_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (search_directory.Text == "Directory Name")
+            {
+                search_directory.Text = "";
+            }
+        }
+
+        private void search_directory_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(search_directory.Text))
+            {
+                search_directory.Text = "Directory Name";
+            }
+        }
+
+        private void search_file_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (search_file.Text == "File Name")
+            {
+                search_file.Text = "";
+            }
+        }
+
+        private void search_file_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(search_file.Text))
+            {
+                search_file.Text = "File Name";
+            }
         }
     }
 }
